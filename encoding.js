1be3cecbac374e146673a1eec82abb54ee251ef1
mytvDor <159150094+mytvDor@users.noreply.github.com>
2024-09-04 07:56:31 +0530

    Update encoding.js

--- encoding.js
+++ encoding.js
@@ -4,19 +4,16 @@ const path = require("path");
 function encodeVideo(inputFile) {
   const outputDir = path.join(__dirname, "output");
 
-  // Command for encoding 720p
   const command720p = `
     ffmpeg -i "${inputFile}" \
     -vf "scale=w=1280:h=720" -c:v libx264 -b:v 2000k -f dash "${outputDir}/720p/720p.mpd"
   `.replace(/\s+/g, " ");
 
-  // Command for encoding 360p
   const command360p = `
     ffmpeg -i "${inputFile}" \
     -vf "scale=w=640:h=360" -c:v libx264 -b:v 500k -f dash "${outputDir}/360p/360p.mpd"
   `.replace(/\s+/g, " ");
 
-  // Execute the 720p encoding
   exec(command720p, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error encoding 720p video: ${error.message}`);
@@ -27,7 +24,6 @@ function encodeVideo(inputFile) {
     console.error("stderr:", stderr);
   });
 
-  // Execute the 360p encoding
   exec(command360p, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error encoding 360p video: ${error.message}`);
@@ -39,4 +35,4 @@ function encodeVideo(inputFile) {
   });
 }
 
-encodeVideo("input/input.mp4"); // Update the path to your input file if necessary
+encodeVideo("input/input.mp4"); 
